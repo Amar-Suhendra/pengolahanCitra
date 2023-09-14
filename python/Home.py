1b14f34013d710689144dac436b733724237ed18
Amar Suhendra <amarsuhendra2@gmail.com>
2023-09-14 19:10:22 +0700

    feat: add web app

---
+++ python/Home.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="🏡",
+)
+
+st.write("# Welcome to Image Processing Web App")
+
+st.markdown(
+    """
+    Image Processing Web App is a web application that allows you to upload an image and apply various image processing techniques to it. this web app is built using [Streamlit](https://streamlit.io/).
+
+    ## How to use?
+    1. Click on the menu on the left to select the image processing technique you want to apply.
+    2. Upload an image.
+    3. Done and dusted! You can now see the result of the image processing technique you selected.
+
+    ## Image Processing Techniques we support
+    - [Histogram Equalization Python](https://opencv24-python-tutorials.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_histograms/py_histogram_equalization/py_histogram_equalization.html)
+
+    ## About us
+    We are a group of students from [Universitas Syiah Kuala](https://usk.ac.id) who are taking a course called Image Processing in the semester of 2023/2024. This web app is our project for the course.
+    ### Our team members
+    - [Amar Suhendra](https://www.instagram.com/suhendra.amar/)
+    - [Teuku Muhammad Roy Adrian](https://www.instagram.com/adr.roy_/)
+    - [Hadafee Mudo](https://www.instagram.com/_dfee__/)
+
+"""
+)
---
+++ python/pages/Histogram_Equalization.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import cv2 as cv
+import numpy as np
+
+st.set_page_config(
+    page_title="Histogram Equalization",
+    page_icon="🏡",
+    layout="wide",
+)
+
+st.write("# Histogram Equalization")
+st.markdown(
+  """
+  Histogram equalization is a method in image processing of contrast adjustment using the image's histogram.
+"""
+)
+
+st.write("## How it works?")
+st.markdown(
+  """
+  Histogram equalization enhances image contrast by adjusting pixel intensities to create a more balanced histogram, improving detail visibility, particularly in images with poor initial contrast. It's a valuable technique in image processing, though it may require careful application, especially in color images where channels are processed independently.
+"""
+)
+
+st.write("## Try it out!")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+
+  # show original image
+  st.write("## Original Image")
+  col1, col2 = st.columns(2)
+  
+  image = cv.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
+  hist = cv.calcHist([image], [0], None, [256], [0, 256])
+  col1.image(image, channels="BGR", width=400)
+  col2.bar_chart(hist)
+
+  # show equalized image
+  st.write("## Equalized Image")
+  col1, col2 = st.columns(2)
+
+  image2 = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+  equalized_image = cv.equalizeHist(image2)
+  equalized_hist = cv.calcHist([equalized_image], [0], None, [256], [0, 256])
+  col1.image(equalized_image, channels = "RGB", width=400)
+  col2.bar_chart(equalized_hist)
