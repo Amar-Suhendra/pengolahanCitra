7e087e4df222c6c5f24c4d5081ca6bc43f44596d
T.M. Roy Adrian <tmroyadrian098@gmail.com>
2023-10-02 01:32:23 +0700

    add cartoonize filter

---
+++ python/pages/5_Cartoonize_Image.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+import tempfile
+
+def cartoonize_image(image_path):
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Resize image untuk mempercepat processing
+    scale_factor = 0.5
+    image_rgb = cv2.resize(image_rgb, None, fx=scale_factor, fy=scale_factor)
+
+    # K-means clustering untuk color quantization
+    num_clusters = 8 
+    pixel_data = image_rgb.reshape((-1, 3))
+    kmeans = KMeans(n_clusters=num_clusters)
+    kmeans.fit(pixel_data)
+    quantized_colors = kmeans.cluster_centers_.astype(int)
+    quantized_image = quantized_colors[kmeans.labels_].reshape(image_rgb.shape)
+
+    grayscale_image = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2GRAY)
+
+    # edge detection threshold menggunakan Canny
+    edge_threshold = 100
+    edges = cv2.Canny(grayscale_image, threshold1=edge_threshold, threshold2=2 * edge_threshold)
+
+    # Invert edges untuk masking
+    edges = 255 - edges
+
+    # menggabungkan cartoonized image dan edge mask
+    cartoonized = cv2.bitwise_and(quantized_image, quantized_image, mask=edges)
+
+    return cartoonized
+
+def main():
+    st.title("Cartoonize Filter with Streamlit")
+
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image:
+        st.image(uploaded_image, caption="Original Image", use_column_width=True)
+
+        if st.button("Cartoonize"):
+            st.spinner("Cartoonizing...")
+            
+            # membuat temporary file untuk menyimpan uploaded image
+            with tempfile.NamedTemporaryFile(delete=False) as temp_image:
+                temp_image.write(uploaded_image.read())
+
+            cartoonized = cartoonize_image(temp_image.name)
+            st.image(cartoonized, caption="Cartoonized Image", use_column_width=True)
+
+if __name__ == "__main__":
+    main()
