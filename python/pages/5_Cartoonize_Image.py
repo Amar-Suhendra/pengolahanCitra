243d741e299a3eb302f1122f848aa8a72164cc1a
T.M. Roy Adrian <tmroyadrian098@gmail.com>
2023-10-09 20:43:39 +0700

    Update 5_Cartoonize_Image.py

--- python/pages/5_Cartoonize_Image.py
+++ python/pages/5_Cartoonize_Image.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 import tempfile
 
+
 def cartoonize_image(image_path):
     image = cv2.imread(image_path)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -13,7 +14,7 @@ def cartoonize_image(image_path):
     image_rgb = cv2.resize(image_rgb, None, fx=scale_factor, fy=scale_factor)
 
     # K-means clustering untuk color quantization
-    num_clusters = 8 
+    num_clusters = 8
     pixel_data = image_rgb.reshape((-1, 3))
     kmeans = KMeans(n_clusters=num_clusters)
     kmeans.fit(pixel_data)
@@ -24,7 +25,8 @@ def cartoonize_image(image_path):
 
     # edge detection threshold menggunakan Canny
     edge_threshold = 100
-    edges = cv2.Canny(grayscale_image, threshold1=edge_threshold, threshold2=2 * edge_threshold)
+    edges = cv2.Canny(grayscale_image, threshold1=edge_threshold,
+                      threshold2=2 * edge_threshold)
 
     # Invert edges untuk masking
     edges = 255 - edges
@@ -34,23 +36,29 @@ def cartoonize_image(image_path):
 
     return cartoonized
 
+
 def main():
     st.title("Cartoonize Filter with Streamlit")
 
-    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    uploaded_image = st.file_uploader(
+        "Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_image:
-        st.image(uploaded_image, caption="Original Image", use_column_width=True)
+        col1, col2 = st.columns(2)
+        col1.header("Original Image")
+        col1.image(uploaded_image, width=400)
 
         if st.button("Cartoonize"):
             st.spinner("Cartoonizing...")
-            
+
             # membuat temporary file untuk menyimpan uploaded image
             with tempfile.NamedTemporaryFile(delete=False) as temp_image:
                 temp_image.write(uploaded_image.read())
 
             cartoonized = cartoonize_image(temp_image.name)
-            st.image(cartoonized, caption="Cartoonized Image", use_column_width=True)
+            col2.header("Cartoonized Image")
+            col2.image(cartoonized, caption="Cartoonized Image", width=400)
+
 
 if __name__ == "__main__":
     main()
